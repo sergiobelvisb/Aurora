195190209044b6fd8ae13b62d9027966fce738bc
Sergio Belvís <sergiobelvisb@gmail.com>
2026-02-19 21:06:06 +0100

    Add files via upload

---
+++ Página Web/Controller/ControllerPrincipal.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+class Principal extends Controller
+{
+    /**
+     * Constructor de la clase.
+     * Llama al constructor de la clase padre.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    /**
+     * Método index que muestra un mensaje de bienvenida.
+     */
+    public function index()
+    {
+        $data = [
+            'extraCSS' => "<link rel='stylesheet' href='public/css/principal.css'>"
+        ];
+        $viewUsuario = new Layout('Principal', $data);
+    }
+}
+
+?>
