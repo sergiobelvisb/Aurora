b1d0f599b7fe530d71ec5bd32789df4193de8ecd
Brian Camba Hipólito <briancambatrabajo@gmail.com>
2026-04-28 11:05:12 +0200

    Update profesionales.blade.php

--- aurora-laravel/aurora-laravel/resources/views/public-pages/profesionales.blade.php
+++ aurora-laravel/aurora-laravel/resources/views/public-pages/profesionales.blade.php
@@ -33,7 +33,7 @@
 
         <div class="row g-4 align-items-center">
             <div class="col-md-6 text-center">
-                {{-- Tu: src="<?=$http->getUrlBase()?>/public/img/panel-control.png" --}}
+                {{-- src="<?=$http->getUrlBase()?>/public/img/panel-control.png" --}}
                 <img src="{{ asset('img/panel-control.png') }}"
                      alt="Panel de Control Médico"
                      class="img-fluid"
